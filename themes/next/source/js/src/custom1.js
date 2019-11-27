1d29f52763edf1fc6c6913af66adcaf6396fde4a
xzjack <zjie1one@gmail.com>
2019-11-28 01:53:25 +0800

    ...

--- themes/next/source/js/src/custom1.js
+++ themes/next/source/js/src/custom1.js
@@ -5,15 +5,18 @@ $(function () {
 	bodyBgs[1] = "/images/2.jpg";
 	bodyBgs[2] = "/images/3.jpg";
 	bodyBgs[3] = "/images/4.jpg";
-	bodyBgs[3] = "/images/5.jpg";
+	bodyBgs[4] = "/images/5.jpg";
+	bodyBgs[5] = "/images/6.jpg";
 
-	var randomBgIndex = Math.round( Math.random() * 4 )+4;
+	var randomBgIndex = Math.round( Math.random() * 6 )+6;
 
     $.backstretch([  
-    	  bodyBgs[randomBgIndex%4],
-    	  bodyBgs[(randomBgIndex+1)%4],
-    	  bodyBgs[(randomBgIndex-1)%4],
-    	  bodyBgs[(randomBgIndex+2)%4],
-    	  bodyBgs[(randomBgIndex-2)%4]
+    	  bodyBgs[randomBgIndex%6],
+    	  bodyBgs[(randomBgIndex+1)%6],
+    	  bodyBgs[(randomBgIndex-1)%6],
+    	  bodyBgs[(randomBgIndex+2)%6],
+    	  bodyBgs[(randomBgIndex-2)%6],
+    	  bodyBgs[(randomBgIndex+3)%6],
+    	  bodyBgs[(randomBgIndex-3)%6]
     ], { duration: 60000, fade: 1000 });  
 });
